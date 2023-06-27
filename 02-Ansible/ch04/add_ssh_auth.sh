125b87748c895d4232e8427aebd40bba30160614
tkwk5445 <tkwk5445@naver.com>
2023-06-27 14:12:38 +0900

    20230627

---
+++ 02-Ansible/ch04/add_ssh_auth.sh
@@ -0,0 +1,9 @@
+# ! /usr/bin/env/ bash
+# ~/.ssh/known_hosts에 ssh 키 값을 저장하는 목적
+#   -p :  password 자동으로 넣어주는 기능     -o strict~:  pingerprint 묻지마라는 뜻
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.11
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.12
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.13
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.14
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.15
+sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.56.16
